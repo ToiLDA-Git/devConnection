be87cd026020ec5db1eb786e337fcd7dd88de6c0
toi.le <toi.le@tbwa.com>
2019-04-09 08:32:36 +0700

    create Front end layout

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      // Footer
+      <footer class="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; 2019 Dev Connector
+      </footer>
+    )
+  }
+}
+
+export default Footer;
